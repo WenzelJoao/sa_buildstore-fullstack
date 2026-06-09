67382614caa13883c63c3a807db556d0a5202da9
wenzeljoao <jvitorwenzel@gmail.com>
2026-06-08 21:00:36 -0300

    feat: configuração inicial do projeto (estrutura do backend e frontend)

---
+++ src/app.js
@@ -0,0 +1,17 @@
+﻿const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  return res.json({
+    status: true,
+    mensagem: 'API da loja de materiais de construcao funcionando',
+    data: null
+  });
+});
+
+module.exports = app;
